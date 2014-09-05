59c4a792da9644204ce0529ca5b085084d5af2ac
Steve Kinney <hello@stevekinney.net>
2014-09-04 21:56:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+The following environment variables need to be set:
+
+* `SLACK_DOMAIN`
+* `SLACK_TOKEN`
+* `TWILIO_ACCOUNT_ID`
+* `TWILIO_AUTH_TOKEN`
+* `TWILIO_PHONE_NUMBER`
---
+++ index.js
@@ -0,0 +1,36 @@
+var restify = require('restify');
+
+var messenger = require('./lib/messenger');
+var slacker = require('./lib/slacker.js');
+
+var server = restify.createServer({
+  name: 'doorbell',
+  version: '0.0.1'
+});
+
+server.pre(restify.pre.sanitizePath());
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+server.use(restify.authorizationParser());
+
+server.post('/messages', function (request, response, next) {
+  var phoneNumber = request.params.From;
+  var message = request.params.Body;
+
+  slacker(message, function (err, slackResponse, body) {
+    if (err) {
+      console.error(err);
+      messenger.send(phoneNumber, 'There was some kind of error.');
+      response.send(500, err);
+    }
+    messenger.send(phoneNumber, 'Got it! Someone will be there shortly!');
+    response.send(200, 'ok');
+  });
+});
+
+if (!module.parent) {
+  server.listen(process.env.PORT || 8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+  });
+}
